ab61d8d44a8281f212a5b12f94cfbe6642dd6fb5
twixer554 <48992822+twixer554@users.noreply.github.com>
2019-03-27 19:08:08 +0500

    Add files via upload

---
+++ coffee/js/script.js
@@ -0,0 +1,22 @@
+$(function () {
+	$("a[href^='#']").click(function () {
+		var _href = $(this).attr("href");
+		$("html, body").animate({ scrollTop: $(_href).offset().top -79 + "px"}, 700);
+		return false;
+	});
+	$("#video__play").click(function () {
+		var dataYoutube = $(this).parents('.js-video').attr('data-youtube');
+		$(this).parents('.js-video').html('<iframe width="100%" height="350" src="https://www.youtube.com/embed/'+ dataYoutube +'?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+	});
+	lightbox.option({
+      'resizeDuration': 0,
+      'wrapAround': true,
+      'fadeDuration': 0,
+      'imageFadeDuration': 0,
+      'positionFromTop': 150
+    });
+    $('.main-title__btn').magnificPopup({
+	  type: 'inline'
+	  // other options
+	});
+});
