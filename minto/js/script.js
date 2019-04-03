e6be443bc24413ed623da753d8c337f4067cf585
twixer554 <48992822+twixer554@users.noreply.github.com>
2019-04-03 16:40:21 +0500

    Minto

---
+++ minto/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('.slick-slider').slick({
+  	arrows: true,
+  	nextArrow: $('.slick-slider-arrow-next'), 
+  	prevArrow: $('.slick-slider-arrow-prev'),
+
+  	responsive: [
+    {
+      breakpoint: 780,
+      settings: {
+        dots: true
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+  });
+  $('.navbar__burger').click(function () {
+		$('.navbar__menu').toggleClass('menu-vis');
+	});
+});
