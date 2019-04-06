75ed255ed4ac06fa42468ab741d0ff71b1d38d82
twixer554 <48992822+twixer554@users.noreply.github.com>
2019-04-06 16:13:37 +0500

    Add files via upload

--- mountains/js/script.js
+++ mountains/js/script.js
@@ -6,10 +6,23 @@ $(document).ready(function(){
 	  dots: true,
 	  nextArrow: $('.slick-slider-arrow-next'), 
 	  prevArrow: $('.slick-slider-arrow-prev'),
+	  autoplay: true,
+	  autoplaySpeed: 3000,
+	   responsive: [
+    {
+      breakpoint: 780,
+      settings: {
+        arrows: false,
+      }
+    }
+  ]
   });
   $("a[href^='#']").click(function () {
 		var _href = $(this).attr("href");
 		$("html, body").animate({ scrollTop: $(_href).offset().top -79 + "px"}, 700);
 		return false;
+	});
+	$('.navbar__burger').click(function () {
+		$('.navbar__nav').slideToggle();
 	});
 });
