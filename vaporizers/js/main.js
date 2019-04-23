58ca4c7d9a17ee40399db92f52fdd981f17363f6
twixer554 <48992822+twixer554@users.noreply.github.com>
2019-04-23 13:40:18 +0500

    Add files via upload

--- vaporizers/js/main.js
+++ vaporizers/js/main.js
@@ -1,11 +1,25 @@
 $(document).ready(function() {
-	// $(function () {
-	// 	$("a[data-target^='anchor']").click(function () {
-	// 		var _href = $(this).attr("href");
-	// 		$("html, body").animate({ scrollTop: $(_href).offset().top -0 + "px"}, 700);
-	// 		return false;
-	// 	});
-	// });
+	$(function () {
+		$("a[data-target^='anchor']").click(function () {
+			var _href = $(this).attr("href");
+			$("html, body").animate({ scrollTop: $(_href).offset().top -50 + "px"}, 700);
+			return false;
+		});
+	});
+	$( '.offer-device-pods').hover(
+		function(){
+	   		$( '.offer-card_pods').fadeIn('slow');
+	    },
+	    function(){
+	    	$( '.offer-card_pods').fadeOut('slow');
+	});
+	$( '.offer-device-liquids').hover(
+		function(){
+	    	$( '.offer-card_liquids').fadeIn('slow');
+	    },
+	    function(){
+	    	$( '.offer-card_liquids').fadeOut('slow');
+	});
 	$('.devices-slider').slick({
 		slidesToShow: 1,
 		centerMode: true,
