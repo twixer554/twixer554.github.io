bbb2c32f6bc27de6f3123328689c9a6231c72a40
twixer554 <48992822+twixer554@users.noreply.github.com>
2019-04-24 17:03:10 +0500

    Add files via upload

--- enjoy/js/main.js
+++ enjoy/js/main.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
     $('.navbar__burger').click(function () {
         $('.navbar__nav').slideToggle();
     });
-    // var w = $(window).width(); 
-    // if (w <= 960) { 
-    //     $('.target-link').click(function () {
-    //         $('.navbar__nav').slideToggle();
-    //         $('.navbar__hidden').trigger('click'); 
-    //         return false;
-    //     });
-    // }
+    var w = $(window).width(); 
+    if (w <= 960) { 
+        $('.target-link').click(function () {
+            $('.navbar__nav').slideToggle();
+            $('.navbar__hidden').trigger('click'); 
+            return false;
+        });
+    }
 });
